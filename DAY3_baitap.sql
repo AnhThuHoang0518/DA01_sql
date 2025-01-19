879bffdeafdebafaca14b153ac1c677248d2927b
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-01-19 23:11:30 +0700

    Create DAY3_baitap.sql

---
+++ DAY3_baitap.sql
@@ -0,0 +1,98 @@
+-- EX1
+SELECT NAME FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
+
+--EX2
+SELECT * FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+
+--EX3
+SELECT CITY, STATE FROM STATION;
+
+--EX4
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY LIKE 'A%'
+OR CITY LIKE 'E%'
+OR CITY LIKE 'I%'
+OR CITY LIKE 'O%'
+OR CITY LIKE 'U%';
+
+--EX5
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY LIKE '%A'
+OR CITY LIKE '%E'
+OR CITY LIKE '%I'
+OR CITY LIKE '%O'
+OR CITY LIKE '%U';
+
+--EX6
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY NOT LIKE 'A%'
+AND CITY NOT LIKE 'E%'
+AND CITY NOT LIKE 'I%'
+AND CITY NOT LIKE 'O%'
+AND CITY NOT LIKE 'U%';
+
+--EX7
+SELECT NAME FROM EMPLOYEE
+ORDER BY NAME;
+
+--EX8
+SELECT NAME FROM EMPLOYEE
+WHERE SALARY > 2000
+AND MONTHS < 10
+ORDER BY EMPLOYEE_ID;
+
+--EX9
+SELECT product_id FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
+
+--EX10
+SELECT name FROM Customer
+WHERE referee_id !=2 OR referee_id IS NULL;
+
+--EX11
+SELECT name, population, area FROM World
+WHERE area >= 3000000
+OR population >= 25000000;
+
+--EX12
+SELECT DISTINCT author_id AS id FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id;
+
+--EX13
+SELECT part, assembly_step FROM parts_assembly
+WHERE finish_date IS NULL;
+
+--EX14
+select * from lyft_drivers
+WHERE yearly_salary <= 30000
+OR yearly_salary >= 70000;
+
+--EX15
+select advertising_channel from uber_advertising
+WHERE money_spent > 100000
+AND year = 2019;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
