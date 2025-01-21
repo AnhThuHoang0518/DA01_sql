d8ae862685c330cb9ab2951da61d87886f74fc8f
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-01-21 19:08:53 +0700

    Update DAY5_baitap.sql

--- DAY5_baitap.sql
+++ DAY5_baitap.sql
@@ -12,7 +12,42 @@ FROM STATION;
 
 
 --EX4
+SELECT 
+ROUND(CAST(SUM(order_occurrences*item_count)/SUM(order_occurrences) AS DECIMAL),1)
+FROM items_per_order;
 
+--EX5
+SELECT candidate_id
+FROM candidates
+WHERE skill IN ('Python','Tableau','PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(*) = 3;
+
+--EX6
+SELECT user_id,
+MAX(DATE(post_date)) - MIN(DATE(post_date)) AS DATE_DIFF
+FROM posts
+WHERE post_date BETWEEN '2021-01-01 0:0:0' AND '2021-12-31 23:59:59'
+GROUP BY user_id
+HAVING COUNT(*) >= 2;
+
+--EX7
+SELECT card_name,
+MAX(issued_amount) - MIN(issued_amount) AS difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC;
+
+--EX8
+SELECT manufacturer,
+COUNT(*) AS drug_count,
+ABS(SUM(total_sales - cogs)) AS total_loss
+FROM pharmacy_sales
+WHERE total_sales - cogs <0
+GROUP BY manufacturer
+ORDER BY drug_count DESC, total_loss DESC;
+
+--EX9
 
 
 
