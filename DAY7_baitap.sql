ccf872579b636e0dbe36eecce2517d9480f620ab
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-01-24 19:31:53 +0700

    Update DAY7_baitap.sql

--- DAY7_baitap.sql
+++ DAY7_baitap.sql
@@ -5,8 +5,18 @@ ORDER BY RIGHT(NAME,3), ID;
 
 --EX2
 SELECT user_id,
-UPPER(LEFT(NAME,1)) || SUBSTRING(LOWER(NAME) FROM 2) AS NAME
+UPPER(LEFT(NAME,1)) || SUBSTRING(LOWER(NAME), 2) AS NAME
 FROM Users
 ORDER BY user_id;
 
 --EX3
+SELECT manufacturer, 
+'$' ||
+ROUND(SUM(total_sales)/1000000)
+|| ' million' AS sales_mil
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER BY SUM(total_sales) DESC, manufacturer;
+
+--EX4
+
