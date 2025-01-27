b98360c5cdf5698cf1b5c52ce323525a1c73bf87
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-01-27 18:18:30 +0700

    Update DAY9_baitap.sql

--- DAY9_baitap.sql
+++ DAY9_baitap.sql
@@ -1,1 +1,31 @@
 --EX1
+SELECT
+sum(CASE
+WHEN device_type = 'laptop' THEN 1
+ELSE 0
+END) as laptop_reviews,
+sum(CASE
+WHEN device_type = 'tablet' or device_type = 'phone' THEN 1
+ELSE 0
+END) as mobile_views
+FROM viewership;
+
+--EX2
+SELECT *,
+CASE
+WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+ELSE 'No'
+END triangle
+FROM Triangle;
+
+--EX3
+SELECT 
+ROUND(CAST(SUM(CASE
+WHEN call_category = 'n/a' OR call_category IS NULL THEN 1
+ELSE 0 
+END) AS DECIMAL)
+/CAST(COUNT(*) AS DECIMAL) * 100, 1)
+FROM callers;
+
+--EX4
+
