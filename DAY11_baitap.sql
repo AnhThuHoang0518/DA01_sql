2ff06ba3ec94ea4a58971ea01190f498e950c4a2
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-02-03 22:33:57 +0700

    Update DAY11_baitap.sql

--- DAY11_baitap.sql
+++ DAY11_baitap.sql
@@ -14,3 +14,37 @@ FROM emails AS A
 LEFT JOIN texts AS B ON A.email_id = B.email_id AND signup_action = 'Confirmed';
 
 --EX3
+SELECT age_bucket,
+ROUND(SUM(CASE 
+WHEN activity_type = 'send' THEN time_spent
+ELSE 0
+END) * 100.0/
+SUM(CASE 
+WHEN activity_type = 'open' THEN time_spent
+WHEN activity_type = 'send' THEN time_spent
+ELSE 0
+END),2) 
+AS send_perc,
+ROUND(SUM(CASE 
+WHEN activity_type = 'open' THEN time_spent
+ELSE 0
+END) * 100.0/
+SUM(CASE 
+WHEN activity_type = 'open' THEN time_spent
+WHEN activity_type = 'send' THEN time_spent
+ELSE 0
+END),2)
+AS open_perc
+FROM activities AS A
+LEFT JOIN age_breakdown AS B ON A.user_id = B.user_id
+GROUP BY age_bucket;
+
+--EX4
+
+
+
+
+
+
+
+
