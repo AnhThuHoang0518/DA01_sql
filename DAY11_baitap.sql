b11ecc2d5d3a44b34b81d7b3dc70efb89a791c77
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-02-03 22:05:07 +0700

    Update DAY11_baitap.sql

--- DAY11_baitap.sql
+++ DAY11_baitap.sql
@@ -7,3 +7,12 @@ ON A.CountryCode = B.Code
 GROUP BY B.Continent;
 
 --EX2
+SELECT 
+ROUND(AVG(CASE
+WHEN A.signup_action = 'Confirmed' THEN 1
+ELSE 0
+END), 2) AS confirm_rate
+FROM texts AS A
+LEFT JOIN emails AS B ON A.email_id = B.email_id;
+
+--EX3
