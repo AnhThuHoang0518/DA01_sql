8b8bd61e269ab921d3f7ecc364fdcb6baf7c5a0f
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-02-05 23:29:13 +0700

    Create DAY13_baitap.sql

---
+++ DAY13_baitap.sql
@@ -0,0 +1,12 @@
+--EX1
+WITH 
+ALL_DUP_JL AS
+(
+SELECT company_id, title, description,
+COUNT(*) AS TIMES_APPEARANCE
+FROM job_listings
+GROUP BY company_id, title, description
+HAVING COUNT(*) >= 2
+)
+SELECT COUNT(DISTINCT company_id) AS duplicate_companies
+FROM ALL_DUP_JL;
