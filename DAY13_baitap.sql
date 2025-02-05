da4a99eff178f829d181cd1034c0426fc602700a
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-02-06 01:11:16 +0700

    Update DAY13_baitap.sql

--- DAY13_baitap.sql
+++ DAY13_baitap.sql
@@ -108,7 +108,20 @@ FROM ALL_TRANS A
 JOIN APPROVE_TRANS B ON A.month = B.month AND A.country = B.country
 
 --EX7
+-- Write your PostgreSQL query statement below
+WITH
+FIRST_YEAR AS
+(SELECT
+product_id,
+MIN(year) AS year
+FROM Sales
+GROUP BY product_id)
+SELECT A.product_id, A.year AS first_year, A.quantity, A.price
+FROM Sales A
+RIGHT JOIN FIRST_YEAR B
+ON A.year = B.year AND A.product_id = B.product_id;
 
+--EX8
 
 
 
