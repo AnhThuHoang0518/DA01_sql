2ee0adcb5580c148ac32cca55b4ed5eaf9761545
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-02-08 11:56:10 +0700

    Update DAY15_baitap.sql

--- DAY15_baitap.sql
+++ DAY15_baitap.sql
@@ -1,1 +1,14 @@
 --EX1
+WITH TWT_curr_year_spend AS
+(SELECT EXTRACT(YEAR FROM transaction_date) AS year,
+product_id,
+SUM(spend) AS curr_year_spend
+FROM user_transactions
+GROUP BY EXTRACT(YEAR FROM transaction_date),
+product_id)
+SELECT *,
+LAG(curr_year_spend) OVER(PARTITION BY product_id ORDER BY year) AS prev_year_spend,
+ROUND((curr_year_spend/LAG(curr_year_spend) OVER(PARTITION BY product_id ORDER BY year) - 1)*100.0,2) AS yoy_rate
+FROM TWT_curr_year_spend;
+
+--EX2
