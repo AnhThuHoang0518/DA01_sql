47866a60d7a9dbc902e333a1c106428ca8e1da36
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-02-08 23:01:22 +0700

    Update DAY16_baitap.sql

--- DAY16_baitap.sql
+++ DAY16_baitap.sql
@@ -1,1 +1,21 @@
 --EX1
+# Write your MySQL query statement below
+WITH 
+FIRST_ORDER_TABLE AS
+(SELECT *
+FROM
+(SELECT *,
+(CASE
+WHEN order_date = customer_pref_delivery_date THEN 'immediate'
+ELSE 'scheduled'
+END) AS TYPE,
+ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date) AS FIRST_ORDER
+FROM Delivery) AS A
+WHERE FIRST_ORDER = 1)
+
+SELECT 
+ROUND((SELECT COUNT(*) FROM FIRST_ORDER_TABLE WHERE TYPE = 'immediate')*100.0/
+COUNT(*),2) AS immediate_percentage
+FROM FIRST_ORDER_TABLE;
+
+--EX2
