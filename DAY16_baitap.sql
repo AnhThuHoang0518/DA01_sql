85bb48d5cd4042140e903128bf01ff1aa4ab29f6
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-02-08 23:29:53 +0700

    Update DAY16_baitap.sql

--- DAY16_baitap.sql
+++ DAY16_baitap.sql
@@ -27,9 +27,24 @@ LEAD(event_date) OVER(PARTITION BY player_id ORDER BY event_date) - event_date A
 FROM Activity)
 
 SELECT 
-ROUND((SELECT COUNT(*) FROM A WHERE DATE_DIFF = 1)*100.0
+ROUND((SELECT COUNT(*) FROM A WHERE DATE_DIFF = 1)
 /COUNT(*),2) AS fraction
 FROM A
 WHERE LOGIN_RANK = 1;
 
 --EX3
+# Write your MySQL query statement below
+WITH CONSECUTIVE_STUDENTS AS
+(SELECT *,
+CEILING(id*1.0/2) AS consecutive_id
+FROM Seat)
+
+SELECT 
+ROW_NUMBER() OVER() AS id,
+student
+FROM (SELECT *,
+ROW_NUMBER() OVER(PARTITION BY consecutive_id ORDER BY id DESC) AS SWITCH1
+FROM CONSECUTIVE_STUDENTS) AS SWITCH_SEAT
+ORDER BY consecutive_id, SWITCH1;
+
+--EX4
