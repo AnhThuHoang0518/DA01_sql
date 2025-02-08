7c879c54f057388ccde6e466451bbb7ba56e036a
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-02-08 20:38:19 +0700

    Update DAY15_baitap.sql

--- DAY15_baitap.sql
+++ DAY15_baitap.sql
@@ -39,3 +39,20 @@ GROUP BY transaction_date, user_id
 ORDER BY transaction_date;
 
 --EX5
+SELECT 
+user_id, tweet_date, ROUND(rolling_avg_3d,2)
+FROM (SELECT *,
+AVG(tweet_count) OVER(PARTITION BY user_id
+                      ORDER BY tweet_date
+                      ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS rolling_avg_3d
+FROM tweets) AS ROLLING_AVG;
+
+--EX6
+
+
+
+
+
+
+
+
