a63c0582e7906615da1200140e4c4453922e6fa4
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-02-15 19:35:20 +0700

    Update DAY21.sql

--- DAY21.sql
+++ DAY21.sql
@@ -19,7 +19,32 @@ ORDER BY A.month_year
 
 /*INSIGHT:
 - Số lượng khách mua hàng và đơn hàng đều tăng theo thời gian
-- Tỉ lệ đơn hàng hoàn thành trên số lượng khách mua hàng chưa cao chỉ ~25%
 */
 
 --2.
+
+WITH A AS
+(SELECT FORMAT_DATE('%Y-%m', created_at) AS month_year,
+SUM(sale_price) AS REV
+FROM bigquery-public-data.thelook_ecommerce.order_items
+GROUP BY FORMAT_DATE('%Y-%m', created_at))
+,
+B AS 
+(SELECT FORMAT_DATE('%Y-%m', created_at) AS month_year,
+COUNT(DISTINCT user_id) AS distinct_users,
+COUNT(*) AS ORDERS
+FROM `bigquery-public-data.thelook_ecommerce.orders` 
+GROUP BY FORMAT_DATE('%Y-%m', created_at))
+
+SELECT A.month_year, distinct_users, 
+ROUND(REV/ORDERS,2) AS average_order_value
+FROM A
+JOIN B ON A.month_year = B.month_year
+ORDER BY A.month_year
+
+/*INSIGHT:
+- Tổng số người dùng khác nhau mỗi tháng tăng theo thời gian nhưng AOV không thay đổi nhiều
+*/
+
+--3.
+
