f12998ee0cc4de779d967ab8e24e9fa61ac0f3d1
Hoàng Anh Thư <146366369+AnhThuHoang0518@users.noreply.github.com>
2025-02-15 19:10:39 +0700

    Update DAY21.sql

--- DAY21.sql
+++ DAY21.sql
@@ -1,1 +1,26 @@
+--1.
+WITH A AS
+(SELECT FORMAT_DATE('%Y-%m', created_at) AS month_year,
+COUNT(DISTINCT user_id) AS total_user
+FROM `bigquery-public-data.thelook_ecommerce.orders` 
+GROUP BY FORMAT_DATE('%Y-%m', created_at)
+ORDER BY month_year)
+,
+B AS 
+(SELECT FORMAT_DATE('%Y-%m', delivered_at) AS month_year,
+COUNT(*) AS total_order
+FROM `bigquery-public-data.thelook_ecommerce.orders` 
+WHERE status = 'Complete'
+GROUP BY FORMAT_DATE('%Y-%m', delivered_at))
 
+SELECT A.month_year, total_user, total_order
+FROM  A
+JOIN B ON A.month_year = B.month_year
+ORDER BY A.month_year
+
+/*INSIGHT:
+- Số lượng khách mua hàng và đơn hàng đều tăng theo thời gian
+- Tỉ lệ đơn hàng hoàn thành trên số lượng khách mua hàng chưa cao chỉ ~25%
+*/
+
+--2.
